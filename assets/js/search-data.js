ac7d018d0621f3b411324c3d26ff22c1eb3126b9
Boskovii <Boskovii@users.noreply.github.com>
2025-12-08 17:28:57 +0000

    deploy: e95777a66a4fa1c5437cdc609b278215a4ba5b34

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Academic and professional experience in mechanical engineering, robotics, and human-robot interaction.",
+          description: "Academic and professional experience in mechanical engineering, robotics, and biomechanics.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
